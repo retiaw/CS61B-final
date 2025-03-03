be2a44a090fbeba5fac53b85f1aece3521599cca
刘先生 <liuxiansheng@waiters-Mac.local>
2025-03-03 17:08:59 +0800

    finished proj1a 01

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,134 @@
+public class ArrayDeque<T> {
+
+    private int head, tail;
+    private int mySize, capacity;
+    private T[] elems;
+
+    private int Head() {
+        return Math.floorMod(head, capacity);
+    }
+
+    private int Tail() {
+        return Math.floorMod(tail, capacity);
+    }
+
+    ArrayDeque() {
+        head = 0;
+        tail = 0;
+
+        capacity = 8;
+        mySize = 0;
+
+        elems = (T[]) new Object[capacity];
+    }
+
+    private void checkResize() {
+        if (capacity < 16 && mySize < capacity) {
+            return ;
+        } else if (4 * mySize >= capacity && mySize < capacity) {
+            return ;
+        }
+        // need more capacity:
+        if (mySize >= capacity) {
+            T[] newElems = (T[]) new Object[capacity * 2];
+            // copy:
+            for (int i = 0; i <= Tail(); i++) {
+                newElems[i] = elems[i];
+            }
+            for (int i = Head(); i < capacity; i++) {
+                newElems[i + capacity] = elems[i];
+            }
+            // modify:
+            elems = newElems;
+            capacity *= 2;
+        }
+        // need less capacity:
+        if (4 * mySize < capacity) {
+            T[] newElems = (T[]) new Object[mySize * 4];
+            // copy:
+            for (int i = 0; i <= Tail(); i++) {
+                newElems[i] = elems[i];
+            }
+            for (int i = capacity - 1, j = mySize * 4 - 1; i >= Head(); i--, j--) {
+                newElems[j] = elems[i];
+            }
+            // modify:
+            capacity = mySize * 4;
+            elems = newElems;
+        }
+    }
+
+    public void addFirst(T value) {
+        checkResize();
+        if (mySize == 0) {
+            elems[head] = value;
+            mySize++;
+        }
+        head--;
+        elems[Head()] = value;
+        mySize++;
+    }
+
+    public void addLast(T value) {
+        checkResize();
+        if (mySize == 0) {
+            elems[tail] = value;
+            mySize++;
+        }
+        tail++;
+        elems[Tail()] = value;
+        mySize++;
+    }
+
+    public boolean isEmpty() {
+        return (mySize == 0);
+    }
+    public int size() {
+        return mySize;
+    }
+
+    public void printDeque() {
+        for (int i = head; i < 0; i++) {
+            System.out.print(elems[Math.floorMod(i, capacity)] + " ");
+        }
+        for (int i = 0; i <= tail; i++) {
+            System.out.print(elems[Math.floorMod(i, capacity)] + " ");
+        }
+    }
+
+    public T removeFirst() {
+        if (mySize == 0) {
+            return null;
+        }
+        T ret = elems[Head()];
+        elems[Head()] = null;
+        head++;
+
+        mySize--;
+        checkResize();
+        return ret;
+    }
+
+    public T removeLast() {
+        if (mySize == 0) {
+            return null;
+        }
+        T ret = elems[Tail()];
+        elems[Tail()] = null;
+        tail--;
+
+        mySize--;
+        checkResize();
+        return ret;
+    }
+
+    public T get(int index) {
+        int i = 0, j = 0;
+        for (i = head, j = 0; i <= tail && j < index; i++, j++);
+        if (i > tail) {
+            return null;
+        } else {
+            return elems[Math.floorMod(i, capacity)];
+        }
+    }
+}
---
+++ proj1a/LinkedListDeque.java
@@ -0,0 +1,107 @@
+public class LinkedListDeque<T> {
+
+    private class Node {
+        T data;
+        Node prev;
+        Node next;
+    }
+    private int mySize;
+    private Node sentinel;
+
+    public LinkedListDeque() {
+        mySize = 0;
+
+        sentinel = new Node();
+        sentinel.data = null;
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(T value) {
+        Node newNode = new Node();
+        // middle:
+        newNode.data = value;
+        newNode.next = sentinel.next;
+        newNode.prev = sentinel;
+        // right:
+        newNode.next.prev = newNode;
+        // sentinel:
+        sentinel.next = newNode;
+
+        mySize++;
+    }
+
+    public void addLast(T value) {
+        Node newNode = new Node();
+        // middle:
+        newNode.data = value;
+        newNode.prev = sentinel.prev;
+        newNode.next = sentinel;
+        // left:
+        newNode.prev.next = newNode;
+        // sentinel:
+        sentinel.prev = newNode;
+
+        mySize++;
+    }
+
+    public boolean isEmpty() {
+        return (mySize == 0);
+    }
+
+    public int size() {
+        return mySize;
+    }
+
+    public void printDeque() {
+        for (Node i = sentinel.next; i != sentinel; i = i.next) {
+            System.out.print(i.data + " ");
+        }
+    }
+
+    public T removeFirst() {
+        if (mySize == 0) {
+            return null;
+        }
+        T ret = sentinel.next.data;
+        sentinel.next = sentinel.next.next;
+        mySize--;
+
+        return ret;
+    }
+
+    public T removeLast() {
+        if (mySize == 0) {
+            return null;
+        }
+        T ret = sentinel.prev.data;
+        sentinel.prev = sentinel.prev.prev;
+        mySize--;
+
+        return ret;
+    }
+
+    // Gets the item at the given index, where 0 is the front, 1 is the next item, and so forth.
+    // If no such item exists, returns null. Must not alter the deque!
+    public T get(int index) {
+        int j = 0;
+        Node i = null;
+        for (j = 0, i = sentinel.next; j < index && i != sentinel; j++, i = i.next);
+
+        return i.data;
+    }
+
+    public T getRecursive(int index) {
+        return getRecursive_helper(index, sentinel.next, 0);
+    }
+
+    private T getRecursive_helper(int index, Node currentNode, int currentIndex) {
+        if (currentNode == sentinel) {
+            return null;
+        } else if (currentIndex == index) {
+            return currentNode.data;
+        } else {
+            return getRecursive_helper(index, currentNode.next, currentIndex + 1);
+        }
+    }
+}
